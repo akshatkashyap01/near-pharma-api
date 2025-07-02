94c5041382eaf43d14f1f5c093fa6a7ebea12fd3
Akshat kashyap <akshatkashyap01@gmail.com.com>
2025-07-02 12:51:31 +0530

    initial commit

---
+++ src/main/java/com/application/nearpharma/NearpharmaApplication.java
@@ -0,0 +1,13 @@
+package com.application.nearpharma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearpharmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NearpharmaApplication.class, args);
+	}
+
+}
